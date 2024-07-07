1e8cdebf3164bfebdad44cd8a86acccef4b7ee8d
Fatih Baytar <matanist@hotmail.com>
2024-07-07 13:36:37 +0100

    Çıkış İşlemleri yapıldı.

--- app/Chat/page.js
+++ app/Chat/page.js
@@ -1,14 +1,15 @@
 "use client";
 import Grid from "@mui/material/Unstable_Grid2";
-import { getSession } from "next-auth/react";
+import { getSession, signOut } from "next-auth/react";
 import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
-import { Autocomplete, Divider, Stack, TextField, Box } from "@mui/material";
-
+import { Autocomplete, Divider, Stack, TextField, Box, AppBar, Toolbar, Typography, IconButton, Menu, MenuItem } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
 export default function Page() {
   const session = getSession();
   const [searchedUsersAndGroups, setSearchedUsersAndGroups] = useState([]);
   const [usersAndGroups, setUsersAndGroups] = useState([]);
+  const [anchorEl, setAnchorEl] = useState(null);
   useEffect(() => {
     session.then((s) => {
       if (s) {
@@ -46,6 +47,15 @@ export default function Page() {
       return x;
     })
     setUsersAndGroups(newUsersAndGroups);
+  }
+  function handleMenu(e){
+    setAnchorEl(e.currentTarget);
+  }
+  function handleClose(){
+    setAnchorEl(null);
+  }
+  function handleExit(e){
+    signOut({callbackUrl:"/", redirect:true});
   }
   return (
     <Grid container height={"100vh"}>
@@ -91,7 +101,30 @@ export default function Page() {
           ))}
         </Grid>
       </Grid>
-      <Grid xl={10} border="solid" borderRadius={2}></Grid>
+      <Grid xl={10} border="solid" borderRadius={2}>
+        <Grid>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{flexGrow:1}}>
+                Menü
+              </Typography>
+              <IconButton size="large" aria-label="account of current user" aria-contols="menu-appbar" aria-haspopup="true" onClick={handleMenu}>
+                <AccountCircle />
+              </IconButton>
+              <Menu id="menu-appbar" anchorOrigin={{vertical:"top", horizontal:"right"}}
+              keepMounted
+              transformOrigin={{vertical:"top", horizontal:"top"}}
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              role="menu" 
+              style={{top:"50px"}}>
+                <MenuItem onClick={handleExit}>Çıkış</MenuItem>
+              </Menu>
+            </Toolbar>
+          </AppBar>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
